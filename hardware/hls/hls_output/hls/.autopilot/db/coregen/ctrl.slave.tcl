4fe1ae4efad9a68c75e404b8b8d10fe666de3006
Jiwoon Lee <jwlee@linux.com>
2025-12-08 21:38:31 +0900

    Remove unused generated files from HLS output
    - Deleted snn_top_hls_regslice_both.v, snn_top_hls_sparsemux_17_3_8_1_1.v, and snn_top_hls.protoinst as they are no longer needed in the project.

--- hardware/hls/hls_output/hls/.autopilot/db/coregen/ctrl.slave.tcl
+++
@@ -1,4 +0,0 @@
-dict set slaves ctrl {ports {ctrl_reg {type i_ap_none width 32} config_reg {type i_ap_none width 32} learning_params {type i_ap_none width 144} status_reg {type o_ap_vld width 32} spike_count_reg {type o_ap_vld width 32} weight_sum_reg {type o_ap_vld width 32} version_reg {type o_ap_vld width 32} reward_signal {type i_ap_none width 8} ap_start {type ap_ctrl width 1} ap_done {type ap_ctrl width 1} ap_ready {type ap_ctrl width 1} ap_idle {type ap_ctrl width 1}} mems {} has_ctrl 1}
-set datawidth 32
-set addrwidth 64
-set intr_clr_mode TOW
